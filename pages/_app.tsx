bff05cc77a5d4480175c16a3921d56e7eeafae58
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2022-11-25 19:43:51 +0000

    Setup TailwindCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { configureAbly } from "@ably-labs/react-hooks";
 
-configureAbly({ authUrl: "/api/ably-token" });
+configureAbly({ authUrl: "http://localhost:3000/api/ably-token" });
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
